9d129f84b933130c29d0fc970fff3cc58f67c022
Verious Smith III <verious.smith@philoveracity.com>
2025-01-27 15:43:53 -0800

    Changes to description and url

--- startempire-wire-network-websockets.php
+++ startempire-wire-network-websockets.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Startempire Wire Network Websockets - Plugin to provide websocket support for the Startempire Wire Network. WordPress-based, server-agnostic, able integrate with any server. Handles real-time features like chat, notifications, and data syncing.
- * Plugin URI:      https://startempire.network/
+ * Plugin URI:      https://startempirewire.network/
  * Description:     Plugin to provide websocket support for the Startempire Wire Network. WordPress-based, server-agnostic, able integrate with any server. Handles real-time features like chat, notifications, and data syncing.
- * Author:          Startempire Network
- * Author URI:      https://startempire.network/
+ * Author:          Startempire Wire
+ * Author URI:      https://startempirewire.com/
  * Text Domain:     sewn-ws
  * Domain Path:     /languages
  * Version:         0.1.0
